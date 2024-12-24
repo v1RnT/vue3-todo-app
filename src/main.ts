9fc17363df54b85ae758671d9ea56485529d9a84
Vitalii <mlineckiyvitaliy@gmail.com>
2024-12-24 05:27:35 +0200

    testing

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import './assets/styles/index.scss'
+
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 
 app.mount('#app')
